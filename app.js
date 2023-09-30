60a5be06c7eee1baff4072f0d26ddec7dff9a12d
daarktangent <aman.jha0409@gmail.com>
2023-09-30 18:55:23 +0530

    completed pagination and delete function

--- app.js
+++ app.js
@@ -39,8 +39,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 let currentPage = 0; 
-function loadVendors(page) {
 
+
+function loadVendors(page) {
+  
     const apiUrl = `http://localhost:8080/page?page=${page}&size=5`; 
 
     fetch(apiUrl, {
@@ -54,19 +56,38 @@ function loadVendors(page) {
         return response.json();
     })
     .then(data => {
-        const vendorList = document.getElementById('vendor-list');
-        vendorList.innerHTML = '';
+        const vendorTable = document.getElementById('vendor-list').getElementsByTagName('tbody')[0];
+        vendorTable.innerHTML = '';
 
         data.content.forEach(vendor => {
-            const listItem = document.createElement('li');
-            listItem.textContent = vendor.vendorName;
-            vendorList.appendChild(listItem);
+            const row = vendorTable.insertRow();
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            const cell3 = row.insertCell(2);
+            const cell4 = row.insertCell(3);
+            const cell5 = row.insertCell(4);
+
+            cell1.textContent = vendor.vendorName;
+            cell2.textContent = vendor.bankAccountNo;
+            cell3.textContent = vendor.bankName;
+            
+            // Edit link
+            const editLink = document.createElement('a');
+            editLink.href = `edit.html?id=${vendor.id}`;
+            editLink.textContent = 'Edit';
+            cell4.appendChild(editLink);
+
+            // Delete link
+            const deleteLink = document.createElement('a');
+            deleteLink.href = `javascript:deleteVendor('${vendor.id}')`; // Call delete function
+            deleteLink.textContent = 'Delete';
+            cell5.appendChild(deleteLink);
         });
 
-
+        // Update current page
         currentPage = data.number;
 
-
+        // Disable/enable pagination buttons based on the page number
         document.getElementById('prev-page').disabled = currentPage === 0;
         document.getElementById('next-page').disabled = currentPage === data.totalPages - 1;
     })
@@ -75,15 +96,44 @@ function loadVendors(page) {
     });
 }
 
+// Function to delete a vendor
+function deleteVendor(id) {
+    const confirmDelete = confirm('Are you sure you want to delete this vendor?');
+    
+    if (confirmDelete) {
+        const apiUrl = `http://localhost:8080/delete/${id}`;
 
+        fetch(apiUrl, {
+            method: 'DELETE',
+            mode: 'cors',
+        })
+        .then(response => {
+            console.log('Response status:', response.status); 
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            if (response.status === 204) {
+                loadVendors(currentPage);
+            } else {
+                throw new Error('Deletion failed');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error); 
+        });
+    }
+}
+
+
+// Event listener for previous page button
 document.getElementById('prev-page').addEventListener('click', () => {
     if (currentPage > 0) {
         loadVendors(currentPage - 1);
     }
 });
 
+// Event listener for next page button
 document.getElementById('next-page').addEventListener('click', () => {
-
     const totalPages = 10; 
 
     if (currentPage < totalPages - 1) {
@@ -91,5 +141,6 @@ document.getElementById('next-page').addEventListener('click', () => {
     }
 });
 
-
+// Initial load of the first page
 loadVendors(currentPage);
+
